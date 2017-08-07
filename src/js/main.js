1ea2a5ccc8c4c5d45c41b27c2fffa2a6600b2305
Basant Besra <besrabasant@gmail.com>
2017-08-07 18:46:30 +0530

    mobile home page completed.

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,17 @@
             function showMenu(button) {
                var menuid = $(button).data('toggle');
                 $(menuid).toggleClass('show');
+                var navItems = $(menuid).find('.nav-item');
+
+                $.each(navItems, function(i, el){
+                    $(el).stop(true, true).removeClass('animateIn');
+                    
+                    if($(menuid).hasClass('show')) {
+                        setTimeout(function(){
+                            $(el).addClass('animateIn');
+                        },500 + ( i * 600 ));
+                    }
+                });
             }
 
             return this;
@@ -35,13 +46,38 @@
     $('.footer').scrollJack({class:'inverse'});
 
     $(document).ready(function(){
-        $('.owl-carousel').owlCarousel({
+
+        var slider = $('.owl-carousel');
+
+        slider.owlCarousel({
             items: 1,
             autoplay: true,
             loop: true,
             animateOut: 'fadeOut',
-            autoplayTimeout: 8000
+            autoplayTimeout: 8000,
+            onInitialized: animateLayers,
+            onTranslated: animateLayers,
+            onTranslate: animateLayers
         });
+
+        function animateLayers(e) {
+            var currentSlide = $(e.target).find('.owl-item').eq(e.item.index).find('.slide');
+            var title = currentSlide.find('.title');
+            var subtitle = currentSlide.find('.subtitle');
+
+            if(e.type == 'translate') {
+                title.removeClass('animated fadeInUp');
+                subtitle.removeClass('animated fadeInUp');
+            }
+             else {
+                title.addClass('animated fadeInUp');
+                setTimeout(function(){
+                    subtitle.addClass('animated fadeInUp');
+                }, 1000)
+            }
+
+        }
+
     });
 
 })(jQuery);
