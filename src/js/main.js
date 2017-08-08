0ea1f6cfe4d10929355718966a0c119c4894bb2b
Basant Besra <besrabasant@gmail.com>
2017-08-08 14:05:51 +0530

    Owl carousel  fixed bg on desktop solved

--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,7 @@
             var screenHeight = $(window).height();
 
             $(window).on('scroll', function(e) {
-                if( (screenWidth < 768 && $(this).scrollTop() > (topOffset-90)) || (screenWidth >= 768 && $(this).scrollTop()+screenHeight-400 > topOffset ) ) {
+                if( (screenWidth < 768 && $(this).scrollTop() > (topOffset-options.mobileOffset)) || (screenWidth >= 768 && $(this).scrollTop() > (topOffset-options.desktopOffset) ) ) {
                     $('body').addClass(options.class);
                 } else {
                     $('body').removeClass(options.class);
@@ -46,7 +46,9 @@
 
     $('.nav-menu-toggle').hamburger();
 
-    $('.footer').scrollJack({class:'inverse'});
+    $('.footer').scrollJack({class:'inverse',mobileOffset: 90, desktopOffset: 300});
+
+    $('.keynote').scrollJack({class:'hide-slider', mobileOffset: 90, desktopOffset: 250});
 
     $(document).ready(function(){
 
@@ -64,15 +66,24 @@
         });
 
         function animateLayers(e) {
+
+            var owlStage = $(e.target).find('.owl-stage');
+            var owlStageX = owlStage.css('transform').split(',')[4];            
+
             var currentSlide = $(e.target).find('.owl-item').eq(e.item.index).find('.slide');
             var title = currentSlide.find('.title');
             var subtitle = currentSlide.find('.subtitle');
+            var background = currentSlide.find('.background');
 
+            
             if(e.type == 'translate') {
                 title.removeClass('animated fadeInUp');
                 subtitle.removeClass('animated fadeInUp');
             }
              else {
+                 if(e.type == 'initialized' ){
+                    repositionSlideBg( $(e.target) );
+                 }
                 title.addClass('animated fadeInUp');
                 setTimeout(function(){
                     subtitle.addClass('animated fadeInUp');
@@ -80,6 +91,21 @@
             }
 
         }
+
+        function repositionSlideBg (slider) {
+            if($(window).width() >= 768) {
+                $.each(slider.find('.owl-item'), function(i, el) {
+                    var slideWidth = $(el).css('width');
+                    var bg = $(el).find('.slide').find('.background');
+                    var slideOffset = parseInt(slideWidth)*(i);
+                    bg.css('background-position', Math.abs(slideOffset)+'px center');
+                });
+            }
+        }
+
+        $(window).on('resize', function() {
+            repositionSlideBg(slider);
+        });
 
     });
 
